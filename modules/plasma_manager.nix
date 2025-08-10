d458857c5956eca746b941b9ddb1550b0549c6e7
Luca Sartore <lucasartore02@gmail.com>
2025-08-10 16:50:45 +0200

    Add default app in bar

--- modules/plasma_manager.nix
+++ modules/plasma_manager.nix
@@ -1,6 +1,8 @@
 { config, pkgs, lib, ... }:
 
 {
+  #Note: this comment return the entire list of options, so that you can discover the syntax and then set it manually
+  #nix run github:nix-community/plasma-manager
 
   programs.plasma = {
     enable = true;
@@ -18,8 +20,30 @@
         widgets = [
           #command reminder to find the widgets names (set them with the UI first)
           #grep "plugin=" ~/.config/plasma-org.kde.plasma.desktop-appletsrc 
-          "org.kde.plasma.kickoff"
-          "org.kde.plasma.icontasks"
+          {
+            name = "org.kde.plasma.kickoff";
+            config = {
+              General = {
+                icon = "nix-snowflake-white";
+                alphaSort = true;
+              };
+            };
+          }
+          {
+            name = "org.kde.plasma.icontasks";
+            config = {
+              General = {
+                # to list all of hte available applications, run
+                # cd /run/current-system/sw/share/applications/ && ls
+                launchers = [
+                  "applications:org.kde.dolphin.desktop"
+                  "applications:google-chrome.desktop"
+                  "applications:code.desktop"
+                  "applications:com.mitchellh.ghostty.desktop"
+                ];
+              };
+            };
+          }
           "org.kde.plasma.marginsseparator"
           "org.kde.plasma.systemmonitor.net"
           "org.kde.plasma.systemmonitor.cpucore"
