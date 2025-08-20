2dff4630f94279a959081c97a8e2cbc731c7dc5c
Luca Sartore <lucasartore02@gmail.com>
2025-08-20 20:00:32 +0200

    better formatting

--- flake.nix
+++ flake.nix
@@ -20,59 +20,49 @@
       inputs.home-manager.follows = "home-manager";
     };
 
-
     # Optional overlay for bleeding‑edge packages
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, plasma-manager, ... }:
-    {
-      nixosConfigurations = {
-        desktop = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            specialArgs = {
-              inherit inputs;
-            };
-            modules = [
-                home-manager.nixosModules.home-manager{
-                  home-manager.useGlobalPkgs = true;
-                  home-manager.useUserPackages = true;
-                  home-manager.users.lucas = ./modules/home.nix;
-                  home-manager.sharedModules = [
-                    plasma-manager.homeManagerModules.plasma-manager
-                  ];
-                  home-manager.extraSpecialArgs = {
-                    inherit inputs;
-                  };
-                }
-                ./modules/all.nix
-                #./modules/home.nix
-                ./hosts/desktop
-            ];
-        };
+  outputs = inputs@{ self, nixpkgs, home-manager, plasma-manager, ... }: {
+    nixosConfigurations = {
+      desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.lucas = ./modules/home.nix;
+            home-manager.sharedModules =
+              [ plasma-manager.homeManagerModules.plasma-manager ];
+            home-manager.extraSpecialArgs = { inherit inputs; };
+          }
+          ./modules/all.nix
+          #./modules/home.nix
+          ./hosts/desktop
+        ];
+      };
 
-        laptop = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            specialArgs = {
-              inherit inputs;
-            };
-            modules = [
-                home-manager.nixosModules.home-manager{
-                  home-manager.useGlobalPkgs = true;
-                  home-manager.useUserPackages = true;
-                  home-manager.users.lucas = ./modules/home.nix;
-                  home-manager.sharedModules = [
-                    plasma-manager.homeManagerModules.plasma-manager
-                  ];
-                  home-manager.extraSpecialArgs = {
-                    inherit inputs;
-                  };
-                }
-                ./modules/all.nix
-                #./modules/home.nix
-                ./hosts/laptop
-            ];
-        };
+      laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.lucas = ./modules/home.nix;
+            home-manager.sharedModules =
+              [ plasma-manager.homeManagerModules.plasma-manager ];
+            home-manager.extraSpecialArgs = { inherit inputs; };
+          }
+          ./modules/all.nix
+          #./modules/home.nix
+          ./hosts/laptop
+        ];
       };
     };
+  };
 }
