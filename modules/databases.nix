086241ad43836ae535292b3f2f3f73a88b5ba764
Luca Sartore <lucasartore02@gmail.com>
2025-09-22 16:18:01 +0200

    Add mariadb config with password

--- modules/databases.nix
+++ modules/databases.nix
@@ -1,12 +1,20 @@
 { config, pkgs, lib, inputs, ... }:
+let
+  mysqlInit = pkgs.writeText "mysql-init.sql" ''
+    CREATE USER IF NOT EXISTS 'lucas'@'localhost' IDENTIFIED BY 'dummy_password';
+    GRANT ALL PRIVILEGES ON *.* TO 'lucas'@'localhost';
+    FLUSH PRIVILEGES;
+  '';
+in {
 
-{
   # mariadb server
+  # host: localhost
+  # port: 3306
+  # username: lucas
+  # password: dummy_password
   services.mysql = {
     enable = true;
     package = pkgs.mariadb;
+    initialScript = mysqlInit;
   };
-
-  users.mysql.user = "lucas";
-  users.mysql.passwordFile = "/run/secrets/mysql-auth-db-passwd";
 }
