b0c1aaeb99e2f1c32401b9bca8bb74d831809d62
Luca Sartore <lucasartore02@gmail.com>
2025-11-19 21:29:00 +0100

    fix: enable kanata autostart

--- modules/kanata.nix
+++ modules/kanata.nix
@@ -8,8 +8,6 @@
   services.kanata.enable = true;
   services.kanata.keyboards.main-keyboard.configFile = lib.mkDefault "/home/lucas/.config/kanata/kanata.kbd";
 
-  # override the default config so that the service does nto start automatically
-  systemd.services.kanata-main-keyboard.wantedBy = lib.mkForce [];
   # override the def config to allow access to the config file
   systemd.services.kanata-main-keyboard.serviceConfig = {
     ProtectHome = lib.mkForce "tmpfs";
